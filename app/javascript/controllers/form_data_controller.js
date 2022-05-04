546836e5b8f954a63f7768475b84df893c9582cb
Pedro Pinheiro <pedro@tidy.com>
2022-05-04 16:40:37 -0400

    Finish implementation

--- app/javascript/controllers/form_data_controller.js
+++ app/javascript/controllers/form_data_controller.js
@@ -8,9 +8,9 @@ export default class extends Controller {
     let newOpts = e.map(
       (e) => {
         let el = document.createElement('option')
-        el['value'] = e.postal_code
+        el['value'] = e.street
         el.text     = e.street
-        //console.log(el)
+        console.log(e)
         //`<option value="${e.postal_code}">${e.street}</option>`
         return el
       }
@@ -25,9 +25,7 @@ export default class extends Controller {
       (e) => {
         let el = document.createElement('option')
         el['value'] = e.city
-        el.text     = `${e.city.slice(0, 1)}${e.city.toLowerCase().slice(1)}`
-        ////console.log(el)
-        ////`<option value="${e.postal_code}">${e.street}</option>`
+        el.text = `${e.city.slice(0, 1)}${e.city.toLowerCase().slice(1)}`
         return el
       }
     )
@@ -35,20 +33,50 @@ export default class extends Controller {
     document.getElementById('city-select').replaceChildren(...newOpts)
   }
 
+  setNumbersData(e) {
+    let newOpts = e.map(
+      (e) => {
+        let el = document.createElement('option')
+        el['value'] = e.number
+        el.text = e.number
+        return el
+      }
+    )
+    //console.log('setNumbersData',e)
+    //console.log(newOpts)
+    document.getElementById('number-selector').replaceChildren(...newOpts)
+  }
+
   citySelectHandler(e) {
     let cityNameUpperCase = e.target.value
-    console.log(`Hello, ${cityNameUpperCase}!`)
+    console.log(`citySelectHandler, ${cityNameUpperCase}!`)
 
-    fetch(`/streets/search/?city=${cityNameUpperCase}`)
+    // Lookup Streets associated with the City selected and
+    // set them to the Streets selector
+    fetch(`/cities/${cityNameUpperCase}/streets/`)
       .then((response) => response.json())
       .then((data) => this.setStreetData(data));
+  }
+
+  streetSelectHandler(e) {
+    let streetNameUpperCase = e.target.value
+    console.log(`streetSelectHandler, ${streetNameUpperCase}!`)
+
+    // Lookup Numbers associated with the Street selected and
+    // set them to the Numbers selector
+    /// streets/ALE%20WEE/numbers/
+    fetch(`/streets/${encodeURIComponent(streetNameUpperCase)}/numbers/`)
+      .then((response) => response.json())
+      .then((data) => this.setNumbersData(data));
   }
 
   postalCodeHandler(e) {
     let postalCode = e.target.value
     if(postalCode.length >= 3) {
-      console.log(`postalCode, ${postalCode}!`)
+      console.log(`postalCodeHandler, ${postalCode}!`)
 
+    // Lookup Cities associated with the Postal Code entered (min 3 length)
+    // set them to the City selector
       fetch(`/postal-codes/${postalCode}/cities/`)
         .then((response) => response.json())
         .then((data) => this.setCityData(data));
--- spec/requests/streets_spec.rb
+++ spec/requests/streets_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "/streets", type: :request do
 
   describe "GET /search" do
     before { Fabricate :city_street_postal_code }
-
     context 'with valid parameters' do
       it 'returns the correct json'do
         get '/streets/search', params: { city: 'LUXEMBOURG'}, headers: headers
@@ -24,6 +23,29 @@ RSpec.describe "/streets", type: :request do
            'debut_tranche' => 'MyString',
            'fin_tranche'   => 'MyString',
            'paire_impaire' => 'p'
+          }
+        )
+      end
+    end
+  end
+
+  describe "GET /streets/:name/numbers/" do
+    before { Fabricate :street }
+    context 'with valid parameters' do
+      it 'returns the correct json'do
+        get "/streets/ALE%20WEE/numbers/", headers: headers
+
+        expect(response).to have_http_status(:ok)
+
+        response_json = JSON.parse(response.body)
+
+        expect(response_json.first).to include(
+          {
+            'number'          => 1,
+            'name'            => 'Ale Wee',
+            'name_upper_case' => 'ALE WEE',
+            'mot_tri'         => 'ALE',
+            'indic_lieu_dit'  => 'N',
           }
         )
       end
